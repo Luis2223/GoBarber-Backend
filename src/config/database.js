a4fbea98b6b210196d9d821ed9eddc0af2d277c3
Luis2223 <luis.santos@grupozelo.com>
2020-07-14 15:39:24 -0300

    [bug-fix] - correção de nomeclaturas nos arquivos

--- src/config/database.js
+++ src/config/database.js
@@ -6,7 +6,7 @@ module.exports = {
   database: 'GoBarber',
   define: {
     timestamps: true,
-    undescored: true,
-    undescoredAll: true,
+    underscored: true,
+    underscoredAll: true,
   },
 };

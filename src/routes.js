979b7db291b79310921da036f98dd0c8250be31b
Luis2223 <luis.santos@grupozelo.com>
2020-07-14 15:54:34 -0300

    #11 - Cadastro de usuários, modelo mvc

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Luis Felipe',
-    email: 'luisfsdsantiago@gmail.com',
-    password_hash: '123455768786',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
